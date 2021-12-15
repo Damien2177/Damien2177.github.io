2c0993da4d865090d8fbeffb13b4e4e276ca07ef
Damien2177 <damienpratt77777@gmail.com>
2021-12-15 09:04:28 +0000

    d

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -24,13 +24,12 @@ var init = function (window) {
         var circle;
         var circles = [];
         // TODO 2 : Create a function that draws a circle 
-        function drawCircle(){
+        function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
             physikz.addRandomVelocity(circle, canvas);
             view.addChild(circle);
             circles.push(circle);
         }
-        
 
         // TODO 3 / 8 : Call the drawCircle() function 
         drawCircle();
