689ec6430d0f91e4ce4dfc516fbb2044e6989504
Damien2177 <damienpratt77777@gmail.com>
2023-08-31 00:32:05 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -36,12 +36,16 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
-
+// TODO 0 complete
   // TODO 1: create a new shape object and add it to the array
-  
-
+  var shape = {
+    color: "blue",
+    shape: "circle",
+    repeat: 3,
+  }
+dataShapes.push(shape);
   // TODO 2: add a new property to all data shapes
-  
+    for (i = 1;dataShapes = i;i++)
 
   // TODO 3-a: add a function that handles the static display type
   
