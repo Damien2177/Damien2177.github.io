bc26d38d41caf730590556ac395be5452c80aced
Damien2177 <damienpratt77777@gmail.com>
2023-10-20 00:16:27 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,14 +10,24 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  let walker = {
+    xcord: 0,
+    ycord: 0,
+    xspeed: 0,
+    yspeed: 0
+  }
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  const KEY = {
+    "LEFT": 37,
+    "UP": 38,
+    "RIGHT": 40,
+    "DOWN": 39
+  }
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -34,8 +44,18 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+      if (event.which === KEY.RIGHT) {
+        console.log("right pressed");
+      } else if (event.which === KEY.LEFT){
+        console.log("left pressed");
+      } else if (event.which === KEY.DOWN){
+        console.log("down pressed");
+      } else if (event.which === KEY.UP) {
+        console.log("up pressed");
+      
+    }
+    console.log()
   }
 
   ////////////////////////////////////////////////////////////////////////////////
